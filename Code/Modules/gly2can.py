a03d3c1d39304a4cd56f297061d51cc598c1d4d2
Adam <s224202@dtu.dk>
2025-04-26 18:21:53 +0200

    got tokenization to work

--- Code/Modules/gly2can.py
+++ Code/Modules/gly2can.py
@@ -1,5 +1,5 @@
 import transformers.models.encoder_decoder as encoder_decoder
-
+from transformers import AutoTokenizer
 class gly2can(encoder_decoder.EncoderDecoderModel):
     """
     A class to represent a glycan translation model.
@@ -9,3 +9,17 @@ class gly2can(encoder_decoder.EncoderDecoderModel):
         super(gly2can, self).__init__(encoder=encoder, decoder=decoder)
         self.encoder = encoder
         self.decoder = decoder
+
+def glycan_tokenizer(glycan_sequences):
+    base_tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-large-uncased")
+    ## quick sanity check prints to make sure this pretraining actually helps
+    print(base_tokenizer.tokenize(glycan_sequences[2]))
+    glycan_tok = base_tokenizer.train_new_from_iterator(get_training_corpus(glycan_sequences=glycan_sequences), 5000)
+    print(glycan_tok.tokenize(glycan_sequences[2]))
+    return glycan_tok
+
+def get_training_corpus(glycan_sequences):
+    return (
+        glycan_sequences[i:1+100]
+        for i in range(0, len(glycan_sequences[0]), 100)
+    )
--- Code/main.py
+++ Code/main.py
@@ -1,23 +1,16 @@
-import transformers
-import pandas as pd
-from tokenizers import Tokenizer
-from tokenizers.models import BPE
-from tokenizers.trainers import BpeTrainer
-from tokenizers.pre_tokenizers import ByteLevel
-import torch
 import numpy as np
-import sys
 from Modules.GlycanDataset import GlycanDataset
-
-
+from Modules.gly2can import gly2can, glycan_tokenizer
+import pandas as pd
 
 def main():
     # Load data
     data = pd.read_csv('./Data/glycan_sequences_full_covers.csv')
     ## limit to 100 sequences for testing
-    data = data.head(100)
+    data = data.head(10)
+    all_sequences = data.drop('glytoucan_ac', axis=1).to_numpy().flatten()
+    np.random.shuffle(all_sequences)
+    tokenizer = glycan_tokenizer(all_sequences)
 
-    encoder
-
-
+np.random.seed(17)
 main()
